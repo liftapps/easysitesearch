ab9541b663161946287253208bb6260f0809809f
lgmys <lgmys@pm.me>
2025-02-06 14:07:28 +0100

    initial

--- src/app.tsx
+++ src/app.tsx
@@ -1,43 +1,165 @@
-import { useState } from 'preact/hooks'
-import preactLogo from './assets/preact.svg'
-import viteLogo from '/vite.svg'
-import './app.css'
+import { useEffect, useReducer, useRef } from 'preact/hooks';
+
+type SearchResult = {
+  title: string;
+  excerpt: string;
+  uri: string;
+};
+
+const runSearch = async (
+  config: { key: string; signal: AbortSignal },
+  query: string,
+) => {
+  if (!query.length) {
+    return [];
+  }
+
+  const url = new URL(`http://localhost:3333/v1/search`);
+
+  url.searchParams.set('key', config.key);
+  url.searchParams.set('query', query);
+
+  const response = await fetch(url, {
+    signal: config.signal,
+  });
+
+  const json = await response.json();
+
+  return json as Array<SearchResult>;
+};
+
+const SearchInput = (props: { onChange: (query: string) => void }) => {
+  return (
+    <input
+      type="search"
+      placeholder="Search anything"
+      onInput={(e) => props.onChange(e.currentTarget.value)}
+    />
+  );
+};
+
+const SearchResultsPreview = (props: { results: SearchResult[] }) => {
+  return (
+    <div>
+      {props.results.map((result) => (
+        <div>
+          <a
+            style={{ display: 'block' }}
+            href={result.uri}
+            dangerouslySetInnerHTML={{ __html: result.title }}
+          ></a>
+          <div
+            id={result.uri}
+            dangerouslySetInnerHTML={{ __html: result.excerpt }}
+          ></div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+type State = {
+  phrase: string;
+  results: SearchResult[];
+  open: boolean;
+};
+
+type Actions =
+  | { type: 'phraseChange'; payload: string }
+  | { type: 'widgetOpen' }
+  | { type: 'widgetClose' }
+  | { type: 'resultsSet'; payload: SearchResult[] };
+
+const initialState: State = { phrase: '', results: [], open: false };
+
+const reducer = (currentState: State, action: Actions): State => {
+  switch (action.type) {
+    case 'widgetOpen': {
+      return {
+        ...currentState,
+        open: true,
+      };
+    }
+    case 'widgetClose': {
+      return initialState;
+    }
+    case 'phraseChange': {
+      return {
+        ...currentState,
+        phrase: action.payload,
+      };
+    }
+    case 'resultsSet': {
+      return {
+        ...currentState,
+        results: action.payload,
+      };
+    }
+    default: {
+      return currentState;
+    }
+  }
+};
 
 export function App() {
-  const [count, setCount] = useState(0)
+  const [state, dispatch] = useReducer<State, Actions>(reducer, initialState);
+
+  const { phrase, results } = state;
+  const timeoutRef = useRef(0);
+
+  useEffect(() => {
+    if (!phrase.length) {
+      dispatch({ type: 'resultsSet', payload: [] });
+      return;
+    }
+
+    const abortController = new AbortController();
+
+    clearTimeout(timeoutRef.current);
+    timeoutRef.current = setTimeout(() => {
+      (async () => {
+        const results = await runSearch(
+          {
+            key: '01bf53f3-5fcf-4da1-ac48-54d89b9f405c',
+            signal: abortController.signal,
+          },
+          phrase,
+        );
+
+        dispatch({ type: 'resultsSet', payload: results });
+      })();
+    }, 240);
+
+    return () => {
+      abortController.abort();
+      clearTimeout(timeoutRef.current);
+    };
+  }, [phrase]);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} class="logo" alt="Vite logo" />
-        </a>
-        <a href="https://preactjs.com" target="_blank">
-          <img src={preactLogo} class="logo preact" alt="Preact logo" />
-        </a>
-      </div>
-      <h1>Vite + Preact</h1>
-      <div class="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/app.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p>
-        Check out{' '}
-        <a
-          href="https://preactjs.com/guide/v10/getting-started#create-a-vite-powered-preact-app"
-          target="_blank"
-        >
-          create-preact
-        </a>
-        , the official Preact + Vite starter
-      </p>
-      <p class="read-the-docs">
-        Click on the Vite and Preact logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <button
+        style={{
+          border: '1px solid black',
+          background: 'none',
+          cursor: 'pointer',
+        }}
+        onClick={() => dispatch({ type: 'widgetOpen' })}
+      >
+        Search...
+      </button>
+
+      {state.open && (
+        <div>
+          <SearchInput
+            onChange={(newPhrase) =>
+              dispatch({ type: 'phraseChange', payload: newPhrase })
+            }
+          />
+
+          <SearchResultsPreview results={results}></SearchResultsPreview>
+        </div>
+      )}
+    </div>
+  );
 }
