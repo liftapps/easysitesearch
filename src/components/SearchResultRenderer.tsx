7e509a1e2660ba855db185a22cf919c6c4970f08
lgmys <lgmys@pm.me>
2025-02-28 12:53:05 +0100

    update css

--- src/components/SearchResultRenderer.tsx
+++ src/components/SearchResultRenderer.tsx
@@ -6,19 +6,25 @@ export const SearchResultRenderer = (props: { result: SearchResult }) => {
     <a className="result" style={{ display: 'block' }} href={props.result.uri}>
       <div>
         <div class="titleWrapper">
-          <span class="category">
-            {DEFAULT_CATEGORIES[props.result.category ?? ''] ??
-              props.result.category}
-          </span>
           <span
             className="title"
             dangerouslySetInnerHTML={{ __html: props.result.title }}
           ></span>
         </div>
+        <div className="uri">
+          {props.result.uri === '/' ? 'homepage' : props.result.uri}
+        </div>
         <div
+          className="excerpt"
           id={props.result.uri}
           dangerouslySetInnerHTML={{ __html: props.result.excerpt }}
         ></div>
+        <div class="tags">
+          <span class="category">
+            {DEFAULT_CATEGORIES[props.result.category ?? ''] ??
+              props.result.category}
+          </span>
+        </div>
       </div>
     </a>
   );
